660ac8dfea6fa9f085cbc8940b7ad83aaa2a7ace
THEGOOODERDER <Aaron.Doolittle@stu.lok12.org>
2024-10-15 20:21:26 -0400

    fixed getting 3rd sample

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutonTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutonTest.java
@@ -22,48 +22,52 @@ public class AutonTest extends AutonBase {
         //CODE
 
         //Deliver Specimen
-        imuDrive(autonFast, 22, 0);
+        imuDrive(autonFast, 21, 0);
         encoderStrafe(autonFast, 14, 5);
-        imuTurn(autonMedium, 90);
-        encoderStrafe(autonMedium, -8, 5);
+        imuTurn(autonFast, 90);
+        encoderStrafe(autonFast, -8, 5);
         sleep(1000);
-        encoderStrafe(autonMedium, 5, 5);
-        imuTurn(autonMedium, -90);
-        imuDrive(autonMedium, -3, 0);
+        encoderStrafe(autonFast, 5, 5);
+        imuTurn(autonFast, -90);
+        imuDrive(autonFast, -3, 0);
 
         //get and deliver first sample
-        imuTurn(autonMedium, -90);
+        imuTurn(autonFast, -90);
         imuDrive(autonFast, 43, 0);
-        imuTurn(autonMedium, 90);
+        imuTurn(autonFast, 90);
         imuDrive(autonFast, 5, 0);
         imuDrive(autonFast, -5, 0);
-        encoderStrafe(autonMedium, -10.5, 5);
+        encoderStrafe(autonFast, -10.5, 5);
         imuDrive(autonFast, -17, 0);
-        imuTurn(autonMedium, 45); //square up with basket
+        imuTurn(autonFast, 45); //square up with basket
         //encoderStrafe(.43,4,5);
         sleep(1000);
-        imuTurn(autonMedium, -45);
+        imuTurn(autonFast, -45);
 
 
         //get and deliver second sample
         imuDrive(autonFast, 20, 0);
         imuDrive(autonFast, -17.25, 0);
-        imuTurn(autonMedium, 45); //square up with basket
+        imuTurn(autonFast, 45); //square up with basket
         sleep(1000);
-        imuTurn(autonMedium, 45);
+        imuTurn(autonFast, 45);
 
-        //get and deliver third sample9
-       /* imuDrive(autonFast,29,0);
-        imuTurn(autonMedium,-90);
-        imuDrive(autonFast,15,0);
-        imuDrive(autonFast,-5,0);
-        imuTurn(autonMedium,90);
-        imuDrive(autonFast,-24,0);
-        encoderStrafe(autonFast,-5,3);
-        imuTurn(autonFast,45); */
+        //get and deliver third sample
+        imuDrive(autonFast,22.25,0);
+        imuTurn(autonFast,-90);
+        imuDrive(autonFast,26.5,0);
+        imuTurn(autonFast,-90);
+        imuDrive(autonFast,24,0);
+        imuTurn(autonFast,90);
+        encoderStrafe(autonFast,5,5);
+        imuDrive(autonFast,-25,0);
+        imuTurn(autonFast,45);
 
         //touch low rung
-        imuDrive(autonFast,25,0);
+        //imuDrive(autonFast,25,0);
+
+
+
 
         //close to wall strafe
       /*encoderStrafe(autonFast, 13, 2);
