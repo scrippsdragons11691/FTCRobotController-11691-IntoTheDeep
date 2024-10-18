e1607fb8705f602a7162d627da69905303732a15
Jason P <greg_prater@yahoo.com>
2024-10-17 20:20:37 -0400

    Initial auton path for specimen

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutonSpecimen.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutonSpecimen.java
@@ -1,7 +1,7 @@
 package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
-@Autonomous(name = "Auton Test", group = "Autons")
+@Autonomous(name = "Auton Specimen", group = "Autons")
 
 public class AutonSpecimen extends AutonBase{
     //This is the auton to deliver samples to the observation zone
@@ -12,7 +12,28 @@ public class AutonSpecimen extends AutonBase{
         initialize();
         waitForStart();
 
-        imuDrive(autonFast, 20, 0);
+        imuDrive(autonMedium, 27, 0);
+        sleep(1000);
+        imuDrive(autonMedium, -3,0);
+        imuTurn(autonMedium,90);
+        imuDrive(autonMedium, 24,0);
+        encoderStrafe(autonMedium, -31, 5);
+        imuDrive(autonMedium,12,0);
+        imuTurn(autonMedium,90);
+        imuDrive(autonMedium,44,0);
+        imuDrive(autonMedium,-44,0);
+        encoderStrafe(autonMedium,-12,5);
+        imuDrive(autonMedium, 44, 0);
+
+        /*
+        imuTurn(autonMedium,180);
+        imuDrive(autonMedium,13.5,0);
+        imuDrive(autonMedium,-10, 0);
+        encoderStrafe(autonMedium,-8,5);
+        imuTurn(autonMedium,180);
+        imuTurn(autonMedium,180);
+        imuDrive(autonMedium,13.5,0);
+        */
     }
 
 }
