7068f2d32fd13c97d6207489b1cb9df614bd640c
THEGOOODERDER <Aaron.Doolittle@stu.lok12.org>
2024-10-19 11:42:51 -0400

    fixed specimen auton

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutonSpecimen.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutonSpecimen.java
@@ -12,18 +12,30 @@ public class AutonSpecimen extends AutonBase{
         initialize();
         waitForStart();
 
-        imuDrive(autonMedium, 27, 0);
+        //push 1st sample to observation zone
+        encoderStrafe(autonMedium,-36,5);
         sleep(1000);
-        imuDrive(autonMedium, -3,0);
-        imuTurn(autonMedium,90);
+        encoderStrafe(autonMedium,9,5);
         imuDrive(autonMedium, 24,0);
-        encoderStrafe(autonMedium, -31, 5);
-        imuDrive(autonMedium,12,0);
+        encoderStrafe(autonMedium, -34, 5);
+        imuDrive(autonMedium,13.5,0);
         imuTurn(autonMedium,90);
-        imuDrive(autonMedium,44,0);
-        imuDrive(autonMedium,-44,0);
-        encoderStrafe(autonMedium,-12,5);
-        imuDrive(autonMedium, 44, 0);
+        imuDrive(autonMedium,48,0);
+
+        imuDrive(autonMedium,-48,0);
+        encoderStrafe(autonMedium,-18,5);
+        imuDrive(autonMedium, 49, 0);
+        imuDrive(autonMedium,-35,0);
+        encoderStrafe(autonMedium,7,5);
+        imuTurn(autonMedium,-90);
+        imuDrive(autonMedium,9,0);
+        imuTurn(autonMedium,90);
+        encoderStrafe(autonMedium,4,5);
+        imuDrive(autonMedium,37,0);
+        imuDrive(autonMedium,-15,0);
+        sleep(3000);
+        imuDrive(autonMedium,15,0);
+
 
         /*
         imuTurn(autonMedium,180);
