a2655e4d9f06cfe8a52dd20c6c3a349dd6d33796
Isaac P <Ixprater@outlook.com>
2024-12-01 20:53:30 -0500

    Added new Auton that parks in the observation zone. updated sample auton for new stategy.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutonSample.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutonSample.java
@@ -2,6 +2,10 @@ package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 
+import org.firstinspires.ftc.teamcode.hardware.ArmPositions;
+import org.firstinspires.ftc.teamcode.hardware.GripperPositions;
+import org.firstinspires.ftc.teamcode.hardware.LifterPositions;
+
 @Autonomous(name = "Auton Sample", group = "Autons")
 public class AutonSample extends AutonBase {
 
@@ -11,62 +15,29 @@ public class AutonSample extends AutonBase {
         initialize();
         waitForStart();
 
-        //Deliver Specimen
-        imuDrive(autonFast, 21, 0);
-        encoderStrafe(autonFast, 14, 5);
-        imuTurn(autonFast, 90);
-        encoderStrafe(autonFast, -8, 5);
+        //Set the arm motor to the drive position
+        intakeArm.moveArmEncoded(ArmPositions.DRIVE);
+
+        //close the gripper
+        gripperServo.moveToPosition(GripperPositions.GRIPPER_CLOSED);
+        specimenLifter.moveLifterEncoded(LifterPositions.TOP);
         sleep(1000);
-        encoderStrafe(autonFast, 5, 5);
-        imuTurn(autonFast, -90);
-        imuDrive(autonFast, -3, 0);
 
-        //get and deliver first sample
-        imuTurn(autonFast, -90);
-        imuDrive(autonFast, 43, 0);
-        imuTurn(autonFast, 90);
-        imuDrive(autonFast, 5, 0);
-        imuDrive(autonFast, -5, 0);
-        encoderStrafe(autonFast, -10.5, 5);
-        imuDrive(autonFast, -17, 0);
-        imuTurn(autonFast, 45); //square up with basket
-        //encoderStrafe(.43,4,5);
+        //hang initial specimen
+        encoderStrafe(autonMedium,-20,5);
+        sleep(500);
+        encoderStrafe(autonSlow,-5.5,5);
         sleep(1000);
-        imuTurn(autonFast, -45);
-
-
-        //get and deliver second sample
-        imuDrive(autonFast, 20, 0);
-        imuDrive(autonFast, -17.25, 0);
-        imuTurn(autonFast, 45); //square up with basket
+        specimenLifter.moveLifterEncoded(LifterPositions.TOP_DELIVER);
         sleep(1000);
-        imuTurn(autonFast, 45);
+        gripperServo.moveToPosition(GripperPositions.GRIPPER_OPEN);
 
-        //get and deliver third sample
-        imuDrive(autonFast,22.25,0);
-        imuTurn(autonFast,-90);
-        imuDrive(autonFast,26.5,0);
-        imuTurn(autonFast,-90);
-        imuDrive(autonFast,24,0);
-        imuTurn(autonFast,90);
-        encoderStrafe(autonFast,5,5);
-        imuDrive(autonFast,-25,0);
-        imuTurn(autonFast,45);
+        //drive to ascent zone
 
-        //touch low rung
-        //imuDrive(autonFast,25,0);
-
-        //close to wall strafe
-        /*
-        encoderStrafe(autonFast, 13, 2);
-        imuDrive(autonFast, 111, 0);
-        */
-
-        //close to submersible strafe
-        /*
-        encoderStrafe(autonFast,-21,3);
-        imuDrive(autonFast,111,0);
-        encoderStrafe(autonFast,23,10);
-        */
+        encoderStrafe(autonMedium,10,5);
+        specimenLifter.moveLifterEncoded(LifterPositions.PICKUP);
+        imuDrive(autonMedium, -25,0);
+        encoderStrafe(autonMedium, 34, 5);
+        imuDrive(autonMedium,8,0);
     }
 }
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutonSpecimenSafe.java
@@ -0,0 +1,87 @@
+package org.firstinspires.ftc.teamcode;
+
+import android.graphics.Color;
+import android.util.Size;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+
+import org.firstinspires.ftc.teamcode.hardware.ArmPositions;
+import org.firstinspires.ftc.teamcode.hardware.GripperPositions;
+import org.firstinspires.ftc.teamcode.hardware.LifterPositions;
+import org.firstinspires.ftc.teamcode.hardware.RobotCameraLight;
+import org.firstinspires.ftc.teamcode.hardware.RobotControlIntake;
+import org.firstinspires.ftc.vision.VisionPortal;
+import org.firstinspires.ftc.vision.opencv.ColorBlobLocatorProcessor;
+import org.firstinspires.ftc.vision.opencv.ColorRange;
+import org.firstinspires.ftc.vision.opencv.ImageRegion;
+import org.opencv.core.RotatedRect;
+
+import java.util.List;
+
+@Autonomous(name = "Auton Specimen Safe", group = "Autons")
+
+public class AutonSpecimenSafe extends AutonBase{
+    //This is the auton to deliver samples to the observation zone and pickup specimens to deliver
+
+    @Override
+    public void runOpMode() {
+
+        initialize();
+        RobotCameraLight cameraLight = new RobotCameraLight(theHardwareMap, this);
+        cameraLight.initialize();
+
+        RobotControlIntake intake = new RobotControlIntake(theHardwareMap, this);
+        intake.initialize();
+
+        //Need some way to determine red vs blue
+
+        //Initialize the camera
+
+        waitForStart();
+
+        //Set the arm motor to the drive position
+        intakeArm.moveArmEncoded(ArmPositions.DRIVE);
+
+        //close the gripper
+        gripperServo.moveToPosition(GripperPositions.GRIPPER_CLOSED);
+        specimenLifter.moveLifterEncoded(LifterPositions.TOP);
+        sleep(1000);
+
+        //hang initial specimen
+        encoderStrafe(autonMedium,-20,5);
+        sleep(500);
+        encoderStrafe(autonSlow,-5.5,5);
+        sleep(1000);
+        specimenLifter.moveLifterEncoded(LifterPositions.TOP_DELIVER);
+        sleep(1000);
+        gripperServo.moveToPosition(GripperPositions.GRIPPER_OPEN);
+
+        //Drive to push samples
+        encoderStrafe(autonMedium,10,5);
+        specimenLifter.moveLifterEncoded(LifterPositions.PICKUP);
+        imuDrive(autonMedium, 25,0);
+        encoderStrafe(autonMedium, -34, 5);
+        imuDrive(autonMedium,12,0);
+
+
+        //we spin around to use the plow on the left side of the robot
+        //imuTurn(autonSlow,90);
+        //imuTurn(autonSlow,90);
+        encoderStrafe(autonMedium,45,5);
+
+        //Push the second sample
+
+        encoderStrafe(autonMedium,-45,5);
+        imuDrive(autonMedium,8,0);
+        encoderStrafe(autonMedium, 45, 5);
+
+
+        //Reset the drive motors to 0 before auton ends
+        specimenLifter.moveLifterEncoded(LifterPositions.BOTTOM);
+        intakeArm.moveArmEncoded(ArmPositions.PICKUP);
+
+        //Give the robot any remaining time to lower the arms to their starting position
+        sleep(5000);
+    }
+
+}
